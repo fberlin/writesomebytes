ab4982a702ce8df4a47428d0b8f00012d4f9cb0b
Fredrik Berlin <fredrik.berlin@live.se>
2020-10-10 15:22:11 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace writesomebytes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] bytes = new byte[256];
+            for (int i = 0; i < 256; i++)
+            {
+                bytes[i] = (byte)i;
+            }
+
+            File.WriteAllBytes("256bytes.txt", bytes);
+        }
+    }
+}
